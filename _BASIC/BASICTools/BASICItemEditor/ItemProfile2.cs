72c6797644262b0a4d106c96ff754857c1742818
Cats <cheesychilli0@gmail.com>
2024-05-25 17:18:35 +0100

    i jsjut realized theres 2 _basic folders (root and in assets)

--- _BASIC/BASICTools/BASICItemEditor/ItemProfile2.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemData_", menuName = "BASIC/Item Editor/Item Profile 2.0 (the sequel)™️", order = 1)]
-public class ItemProfile2 : ScriptableObject
-{
-    public ItemInfo[] items;
-    public string profileName;
-}
